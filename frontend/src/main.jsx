4b65a32889f9e2de942d38fb7ad521f91a5b2026
Joy <joycseru@gmail.com>
2024-12-01 20:39:07 +0600

    add backend api from render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import axios from 'axios'
-axios.defaults.baseURL = "http://localhost:5000"; 
+//axios.defaults.baseURL = "http://localhost:5000"; 
+axios.defaults.baseURL = "https://mcq-app-api.onrender.com/"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
